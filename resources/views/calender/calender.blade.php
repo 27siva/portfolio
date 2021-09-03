7f5e65851ae8f7153a32d70c2ad6cfd941b11195
27siva <izmks123@gmail.com>
2021-09-03 22:29:27 +0900

    9/3更新分

--- resources/views/calender/calender.blade.php
+++ resources/views/calender/calender.blade.php
@@ -6,86 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>2021 Calender</title>
-    <link rel="stylesheet" type="text/css" href="calender.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/calender.css')}}">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/calender.js') }}" defer></script>
 
     <style>
-        .calender-container {
-    width: 500px;
-    margin: 0;
-    border-radius: 5px;
-    background-color: bisque;
-    color: crimson;
-}
-
-.h1 {
-height: 80px;
-line-height: 80px;
-text-align: center;
-font-size: 24px;
-margin: 0;
-}
-
-.calender {
-    padding: 0 30px 30px 30px;
-}
-/* .calender *:first-child {
-    color: red;
-}
-
-.calender *:last-child {
-    color: blue; */
-/* } */
-
-table {
-    width: 100%;
-}
-
-tr,td {
-    text-align: center;
-    padding: 10px;
-}
-tr {
-    font-weight: normal;
-    font-size: 14px;
-}
-
-td {
-    font-weight: bold;
-}
-
-.day {
-    color: olivedrab;
-}
-
-.day *:first-child {
-    color: red;
-}
-.day *:last-child {
-    color: blue;
-}
-
-#overlay{
-    position: absolute;
-    left: 0; top: 0;
-  width: 100%; height: 100%;
-  background: rgba(100, 100, 100, .8);
-  z-index: 2147483647;
-}
-
-.hidden{
-    display: none;
-}
-
-.popup {
-    display: none;
-    height: 100vh;
-    width: 100%;
-    background: black;
-    
-}
+  
 
     </style>
 </head>
